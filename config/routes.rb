9ac0ba4641307c49e2e5c424ee1117ff59d96697
Michael O'Keefe <okeefm@rpi.edu>
2013-10-28 16:01:55 -0400

    Updated config/routes.rb

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,9 @@ Cprsite::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   
-  resources :course_types, :users
+  resources :course_types, :users, :user_sessions
+  
+  resources :account, :controller => "users"
     
   get "welcome/handsonly"
   get "welcome/learn"
