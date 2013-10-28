77db3624b489da21e4211e1f58650860cde2cc18
Michael O'Keefe <okeefm@rpi.edu>
2013-10-28 19:11:26 -0400

    Added routes for courses and organizations to routes

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Cprsite::Application.routes.draw do
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   
-  resources :course_types, :users, :user_sessions
+  resources :course_types, :users, :user_sessions, :courses, :organizations
   
   resources :account, :controller => "users"
     
