f7f26334e1278723efb674373bcf99d10ad11672
Michael O'Keefe <okeefm@rpi.edu>
2013-10-29 11:08:47 -0400

    Updated routes to include privilege level adding for users

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,17 @@
 Cprsite::Application.routes.draw do
+  resources :privilege_levels
+
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
   
-  resources :course_types, :users, :user_sessions, :courses, :organizations
+  resources :course_types, :user_sessions, :courses, :organizations, :privilege_levels
   
+  resources :users do
+	member do
+		get 'privilege_levels'
+		patch 'update_user_privilege_levels'
+	end
+  end
   resources :account, :controller => "users"
     
   get "welcome/handsonly"
