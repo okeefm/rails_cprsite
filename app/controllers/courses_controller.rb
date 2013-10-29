38d06fd8b2a4b666561f60c64b6d74ca1e098cfd
Michael O'Keefe <okeefm@rpi.edu>
2013-10-29 03:53:20 -0400

    Added and modified many course files

--- app/controllers/courses_controller.rb
+++ app/controllers/courses_controller.rb
@@ -1,2 +1,49 @@
 class CoursesController < ApplicationController
+	def new
+		@course = Course.new
+	end
+	
+	def create
+		@course = Course.new(course_params)
+		
+		if @course.save
+			redirect_to @course
+		else
+			render 'new'
+		end
+	end
+	
+	def show
+	  @course = Course.find(params[:id])
+	end
+	
+	def index
+		@courses = Course.all
+	end
+	
+	def edit
+		@course = Course.find(params[:id])
+	end
+	
+	def update
+		@course = Course.find(params[:id])
+		
+		if @course.update(course_params)
+			redirect_to @course
+		else
+			render 'edit'
+		end
+	end
+	
+	def destroy
+		@course = Course.find(params[:id])
+		@course.destroy
+		
+		redirect_to courses_path
+	end
+	
+	private
+		def course_params
+			params.require(:course).permit(:name, :start, :end, :max_capacity, :web_description, :email_description, :class_location, :organization_id, :instructor_ids=>[])
+		end
 end
--- app/models/course.rb
+++ app/models/course.rb
@@ -1,2 +1,13 @@
 class Course < ActiveRecord::Base
+	belongs_to :organization
+	has_and_belongs_to_many :instructors, class_name: "User"
+	#has_and_belongs_to_many :attendees, class_name: "User"
+	
+	validates :name, presence: true
+	validates :start, presence: true
+	validates :end, presence: true
+	validates :class_location, presence: true
+	validates :organization_id, presence: true
+	validates :max_capacity, presence: true, numericality: {only_integer: true, greater_than: 0}
+	
 end
