66616f2a126f97896e323fc30bff6bcd80de2362
Michael O'Keefe <okeefm@rpi.edu>
2013-10-29 03:57:17 -0400

    Added many privilege level files

---
+++ app/models/privilege_level.rb
@@ -0,0 +1,3 @@
+class PrivilegeLevel < ActiveRecord::Base
+	has_and_belongs_to_many :users
+end
---
+++ test/models/privilege_level_test.rb
@@ -0,0 +1,7 @@
+require 'test_helper'
+
+class PrivilegeLevelTest < ActiveSupport::TestCase
+  # test "the truth" do
+  #   assert true
+  # end
+end
